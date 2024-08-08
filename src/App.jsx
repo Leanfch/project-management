ddf16c0f035f93b42cbe1b095742faa46308a428
Leandro Fernandez <leandrof@thinkion.com.ar>
2024-08-08 15:07:57 -0300

    managing-state-switch-between-comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import NewProject from "./components/NewProject";
-import NoProjectSelected from "./components/NoProjectSelected";
-import ProjectsSideBar from "./components/ProjectsSideBar";
+import NewProject from "./components/NewProject"
+import NoProjectSelected from "./components/NoProjectSelected"
+import ProjectsSideBar from "./components/ProjectsSideBar"
+import { useState } from "react"
 
 function App() {
-  return (
-    <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar />
-      <NewProject />
-      <NoProjectSelected />
-    </main>
-  );
+    const [projectState, setProjectState] = useState({
+        selectedProjectId: undefined,
+        projects: [],
+    })
+
+    function handleStartAddProject() {
+        setProjectState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null,
+            }
+        })
+    }
+
+    let content;
+
+    if(projectState.selectedProjectId === null) {
+        content = <NewProject/>;
+    }else if (projectState.selectedProjectId === undefined) {
+        content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+    }
+    return (
+        <main className="h-screen my-8 flex gap-8">
+            <ProjectsSideBar onStartAddProject={handleStartAddProject}/>
+            {content}
+        </main>
+    )
 }
 
-export default App;
+export default App
